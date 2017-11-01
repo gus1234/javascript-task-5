7df0ac433bb24721bbc1c05b9ad957d5467718bd
Gusarov Vadim <gusarovvadim1996@gmail.com>
2017-11-01 19:13:31 +0500

    add star.

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = false;
+getEmitter.isStar = true;
 module.exports = getEmitter;
 
 /**
@@ -24,11 +24,7 @@ function getEmitter() {
          * @returns {Object}
          */
         on: function (event, context, handler) {
-            if (subscribers.hasOwnProperty(event)) {
-                subscribers[event].push({ 'student': context, 'handler': handler });
-            } else {
-                subscribers[event] = [{ 'student': context, 'handler': handler }];
-            }
+            subscribering(subscribers, event, context, handler, Infinity, 1);
 
             return this;
         },
@@ -50,7 +46,7 @@ function getEmitter() {
                 }
             }
             for (let _event of events) {
-                subscribers[_event] = subscribers[_event]
+                subscribers[_event].students = subscribers[_event].students
                     .filter(element => element.student !== context);
             }
 
@@ -66,8 +62,16 @@ function getEmitter() {
             let events = getEvents(event);
             for (let item of events) {
                 if (subscribers.hasOwnProperty(item)) {
-                    subscribers[item].forEach(function (element) {
-                        element.handler.call(element.student);
+                    subscribers[item].count += 1;
+                    subscribers[item].students.forEach(function (element) {
+                        let number = 1 +
+                            Math.floor(subscribers[item].count / element.frequency) *
+                                element.frequency;
+                        if (element.count > 0 && (element.frequency === 1 ||
+                            number === subscribers[item].count)) {
+                            element.handler.call(element.student);
+                            element.count -= 1;
+                        }
                     });
                 }
             }
@@ -82,9 +86,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            times = times <= 0 ? Infinity : times;
+            subscribering(subscribers, event, context, handler, times, 1);
+
+            return this;
         },
 
         /**
@@ -93,10 +101,14 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
-         * @param {Number} frequency – как часто уведомлять
+         * @param {Number} _frequency – как часто уведомлять
+         * @returns {Object}
          */
-        through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+        through: function (event, context, handler, _frequency) {
+            _frequency = _frequency <= 0 ? 1 : _frequency;
+            subscribering(subscribers, event, context, handler, Infinity, _frequency);
+
+            return this;
         }
     };
 }
@@ -111,4 +123,29 @@ function getEvents(event) {
     events.reverse();
 
     return events;
+}
+
+function subscribering(...parameters) {
+    let subscribers;
+    let event;
+    let context;
+    let _handler;
+    let _count;
+    let _frequency;
+    [subscribers, event, context, _handler, _count, _frequency] = parameters;
+    if (subscribers.hasOwnProperty(event)) {
+        subscribers[event].students.push({
+            student: context,
+            handler: _handler,
+            count: _count,
+            frequency: _frequency });
+    } else {
+        subscribers[event] = {
+            count: 0,
+            students: [{
+                student: context,
+                handler: _handler,
+                count: _count,
+                frequency: _frequency }] };
+    }
 }
